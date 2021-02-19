15d20d50fbc55c5d4e143b23164fa1c03af76ee8
Brian York <briandavidyork@gmail.com>
2021-02-19 11:37:58 -0500

    Define Server and endpoints;

---
+++ src/controller.js
@@ -0,0 +1,9 @@
+const users = require('./../data/users.json');
+
+exports.getUsers = (req, res) => {};
+
+exports.createUser = (req, res) => {};
+
+exports.updateUser = (req, res) => {};
+
+exports.deleteUser = (req, res) => {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-const users = require('./../data/users.json');
+let express = require("express");
+let app = express();
+const controller = require('./controller');
+
+app.listen(8000, () => {
+  console.log("Server running on port 3000");
+});
+
+// endpoints
+app.get('/api/users', controller.getUsers);
+app.post('/api/users', controller.createUser);
+app.put('/api/users', controller.updateUser);
+app.delete('/api/users', controller.deleteUser);
+
+module.exports = app;
