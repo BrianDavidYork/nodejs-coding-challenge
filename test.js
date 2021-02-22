99fe58a2cb047e7d23dd94d5c60d67084842e351
Brian York <briandavidyork@gmail.com>
2021-02-21 20:00:09 -0500

    POST tests;

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const { assert } = chai;
 const app = require("./src/index.js");
 chai.use(chaiHttp);
 
-describe("GET users", () => {
-  it("success", (done) => {
+describe("API tests", () => {
+  it("GET success", (done) => {
     chai
       .request(app)
       .get('/api/users')
@@ -17,7 +17,7 @@ describe("GET users", () => {
       });
   });
 
-  it("success sortby ascending", (done) => {
+  it("GET success sortby ascending", (done) => {
     chai
       .request(app)
       .get('/api/users?sortBy=email&sortDirection=ascending')
@@ -30,7 +30,7 @@ describe("GET users", () => {
       });
   });
 
-  it("success sortby descending", (done) => {
+  it("GET success sortby descending", (done) => {
     chai
       .request(app)
       .get('/api/users?sortBy=email&sortDirection=descending')
@@ -43,7 +43,7 @@ describe("GET users", () => {
       });
   });
 
-  it("success pagination", (done) => {
+  it("GET success pagination", (done) => {
     chai
       .request(app)
       .get('/api/users?page=1&limit=12')
@@ -52,6 +52,44 @@ describe("GET users", () => {
         assert.ok(res.body.data);
         assert.isArray(res.body.data);
         assert.lengthOf(res.body.data, 12);
+        done()
+      });
+  });
+
+  it("POST failure", (done) => {
+    chai
+      .request(app)
+      .post('/api/users')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({name: 'brian', email: 'brian@brian.com'})
+      .end((err, res) => {
+        assert.equal(res.statusCode, 400);
+        done()
+      });
+  });
+
+  it("POST success", (done) => {
+    chai
+      .request(app)
+      .post('/api/users')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({name: 'brian', email: 'brian@brian.com', dateOfBirth: 'yesterday', phoneNumber: 300, address: 'homeless'})
+      .end((err, res) => {
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.body.message, "New user created!");
+        done()
+      });
+  });
+
+  it("GET success POSTed user", (done) => {
+    chai
+      .request(app)
+      .get('/api/users?match[email]=brian@BRIAN.com')
+      .end((err, res) => {
+        assert.equal(res.statusCode, 200);
+        assert.ok(res.body.data);
+        assert.isArray(res.body.data);
+        assert.lengthOf(res.body.data, 1);
         done()
       });
   });
